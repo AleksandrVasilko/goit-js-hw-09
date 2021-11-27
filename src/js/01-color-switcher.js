feaa2c0e542e2af63450898496d19eb8016a268e
AleksandrVasilko <avasilko7@gmail.com>
2021-11-27 10:20:44 +0200

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
+stop.setAttribute("disabled", "disabled");
+
+start.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        start.setAttribute("disabled", "disabled");
+        stop.removeAttribute("disabled");
+        const colorSet = getRandomHexColor();
+        bodyEl.style.background = colorSet;
+    },1000);
+});
+
+stop.addEventListener('click', () => {
+    clearInterval(timerId);
+    start.removeAttribute("disabled");
+    stop.setAttribute("disabled", "disabled");
+})
