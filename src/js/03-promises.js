f53e9a757dc1a8d8d0afdbbde0e709c1c0f49b89
AleksandrVasilko <avasilko7@gmail.com>
2021-11-30 21:13:35 +0200

    Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,64 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+formForInputValues: document.querySelector('.form'),
+inputValueOfFirstDelay: document.querySelector('[name="delay"]'),
+inputValueOfDelayStep: document.querySelector('[name="step"]'),
+inputValueOfAmount: document.querySelector('[name="amount"]'),
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+        // Reject
+      }
+      }, delay);
+    });
 }
+ 
+
+const generatePromis = () => {
+  const refpromis = {
+    delay: Number(refs.inputValueOfFirstDelay.value),
+    delayStep: Number(refs.inputValueOfDelayStep.value),
+    amount: Number(refs.inputValueOfAmount.value),
+  }
+    for (let i = 1; i <= refpromis.amount; i += 1) {
+      createPromise(i, refpromis.delay)
+        .then(({ position, delay }) => {
+          Notify.success(
+                `Fulfilled promise ${position} in ${delay}ms`,
+                {
+                  timeout: 5000,
+                },
+          );
+        })
+        .catch(({ position, delay }) => {
+            Notify.failure(
+                `Rejected promise ${position} in ${delay}ms`,
+                {
+                  timeout: 5000,
+                },
+          );
+        });
+      refpromis.delay += refpromis.delayStep;
+    }
+}
+
+
+
+const submitForm = (e) => {
+  e.preventDefault();
+  generatePromis()
+  e.currentTarget.reset()
+}
+
+
+refs.formForInputValues.addEventListener('submit', submitForm)
